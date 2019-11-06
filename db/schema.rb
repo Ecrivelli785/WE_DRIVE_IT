19b382473f9845f33d779970ee633aa6fc67f04c
enzo crivelli <enzo_crivelli97@hotmail.com>
2019-11-06 18:06:17 -0300

    last

--- db/schema.rb
+++ db/schema.rb
@@ -88,9 +88,9 @@ ActiveRecord::Schema.define(version: 2019_11_06_183238) do
     t.bigint "service_type_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "total_time_ride"
     t.string "content"
     t.integer "total_price_cents", default: 0, null: false
+    t.integer "total_time_ride"
     t.index ["service_type_id"], name: "index_rides_on_service_type_id"
     t.index ["user_id"], name: "index_rides_on_user_id"
   end
