7118f3ec82965e5f45582103e0b1eea379863602
gaspar baldunciel <Gasparbaldunciel@gmail.com>
2019-11-11 20:13:11 -0300

    giving js to new

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -6,10 +6,14 @@ const initAutocomplete = () => {
 
   if (addressInput) {
     addressInput.forEach((input) => {
-      places({ container: input });
+      places({
+        container: input,
+        // TODO: get from .env
+        appId: 'plURO0PTZUUG',
+        apiKey: '886e6b850e09a8b1e7086ef43ef3eac6',
+      });
     })
   }
 };
-
 
 export { initAutocomplete };
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,4 +1,7 @@
 import mapboxgl from 'mapbox-gl';
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions'
+import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
+
 
 const mapElement = document.getElementById('map');
 
@@ -21,16 +24,89 @@ const addMarkersToMap = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+  map.fitBounds(bounds, { padding: 70, duration: 0, zoom: 4, center: [-58.37723, -34.61315] });
 };
 
 const initMapbox = () => {
   if (mapElement) {
     const map = buildMap();
+
+    const directions = new MapboxDirections({
+      accessToken: mapboxgl.accessToken,
+      unit: 'metric',
+      profile: 'mapbox/driving',
+      controls: {
+        inputs: false,
+        instructions: false,
+        profileSwitcher: true
+      }
+    });
+
+    map.addControl(directions, 'top-left');
+
+    // Add geolocate control to the map.
+    map.addControl(new mapboxgl.GeolocateControl({
+      positionOptions: {
+      enableHighAccuracy: true
+      },
+      trackUserLocation: true
+    }));
+      // directions.setOrigin("Santa Fe, Argentina");
+      // directions.setDestination("Buenos Aires, Argentina");
+
+
     const markers = JSON.parse(mapElement.dataset.markers);
-    addMarkersToMap(map, markers);
-    fitMapToMarkers(map, markers);
+    if (markers) {
+      addMarkersToMap(map, markers);
+      fitMapToMarkers(map, markers);
+    }
+
+
+map.on('load', () => {
+
+
+    if (serviceType != "One way trip") {
+
+      const originAddress = document.getElementById("ride_steps_attributes_0_address")
+      const firstPlaceAddress = document.getElementById("ride_steps_attributes_1_address")
+      const returnAddress = document.getElementById("ride_steps_attributes_2_address")
+
+
+      originAddress.addEventListener('focusout', (event) => {
+        directions.setOrigin(event.target.value);
+        console.log(directions.getWaypoints())
+      });
+
+      firstPlaceAddress.addEventListener('focusout', (event) => {
+        directions.setDestination(event.target.value);
+        console.log(event)
+        console.log(directions.getWaypoints())
+      });
+
+      returnAddress.addEventListener('focusout', (event) => {
+        directions.setDestination(event.target.value);
+        console.log(directions.getWaypoints())
+      });
+    } else {
+
+      const originAddress = document.getElementById("ride_steps_attributes_0_address")
+      const destinationAddress = document.getElementById("ride_steps_attributes_1_address")
+
+      originAddress.addEventListener('focusout', (event) => {
+        directions.setOrigin(event.target.value);
+      });
+
+      destinationAddress.addEventListener('focusout', (event) => {
+        directions.setDestination(event.target.value);
+      });
+    }
+});
+
   }
 };
+
+
+
+
 
 export { initMapbox };
