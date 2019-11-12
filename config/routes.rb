d7b841e69ed071a26f790d0f3fc79ce24832e7f7
Gaston Lascano <gastonlascano@gmail.com>
2019-11-12 09:10:15 -0300

    lunes por la tarde

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     get 'payments/add_card', to: 'payments#add_card', as: :add_card
     post 'add_payment', to: 'payments#add_payment', as: :procesar
     resources :payments, only: [:new, :create]
-    resources :reviews, only: :create
+    resources :reviews, only: [:new, :create]
   end
 
   resources :questions, only: :index
