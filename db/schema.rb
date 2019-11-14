bc68eb2705793bd0601ef06bb0488ec1954b62a4
enzo crivelli <enzo_crivelli97@hotmail.com>
2019-11-14 15:43:28 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_12_182653) do
+ActiveRecord::Schema.define(version: 2019_11_14_163638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -133,6 +133,7 @@ ActiveRecord::Schema.define(version: 2019_11_12_182653) do
     t.string "mp_customer_id"
     t.date "birthday"
     t.string "license"
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
