47d481685022767c066abdd22889d502bc3e4f35
gaspar baldunciel <Gasparbaldunciel@gmail.com>
2019-11-14 18:11:45 -0300

    added mails wlcome and reservation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,10 @@
 class UserMailer < ApplicationMailer
   def welcome
     @user = User.last
-    mail(to: @user.email, subject: "Bienvenido #{@user.full_name.capitalize}")
+    if @user.full_name
+      mail(to: @user.email, subject: "Bienvenido #{@user.full_name.capitalize}")
+    else
+      mail(to: @user.email, subject: "Bienvenido #{@user.email}")
+    end
   end
 end
