60e67b1d7d46678a21b6b74f3a8869c89f81b967
gaspar baldunciel <Gasparbaldunciel@gmail.com>
2019-12-02 21:52:35 -0300

    hasta aca

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
     # if condition
     # Le sacamos la segunda aprte de la query porqeu la seed no tienen mercado pago ID y rompía todo.
     # @rides = Ride.joins(:user).where(rides: { status: "PENDIENTE" }).where.not(users: { mp_card_id: nil })
-    @rides = Ride.joins(:user).where(rides: { status: "PENDIENTE" })
+    @rides = Ride.joins(:user).where(rides: { status: "PENDIENTE" }).where.not(users: { mp_card_id: nil })
     @myrides = Ride.where(driver: current_user)
   end
 
